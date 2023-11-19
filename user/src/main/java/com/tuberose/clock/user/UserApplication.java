0cc141dbf034f9160d51c61cab5f3a74f8eb1e91
tuberose <zhong.ht@qq.com>
2023-11-19 23:16:34 +0800

    create the user module

---
+++ user/src/main/java/com/tuberose/clock/user/UserApplication.java
@@ -0,0 +1,19 @@
+package com.tuberose.clock.user;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
+
+@SpringBootApplication
+@Slf4j
+public class UserApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication application = new SpringApplication(UserApplication.class);
+        Environment env = application.run(args).getEnvironment();
+        log.info("user module is running.\thttp://127.0.0.1:{}", env.getProperty("server.port"));
+    }
+
+}
